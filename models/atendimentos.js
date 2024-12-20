290669cdd5379d32a0073de392e3bb67ab3a85e1
GuinhoFSilva <guilherme.silva@sptech.school>
2024-12-20 09:58:11 -0300

    Formatando e salvando as datas

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,14 +1,19 @@
-const conexao = require("../database/conexao")
+const moment = require('moment');
+
+const conexao = require("../database/conexao");
 
 class Atendimento {
     adicionar(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const dataAtendimento = moment(atendimento.dataAtendimento, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        const atendimentoDatado = {...atendimento, dataCriacao, dataAtendimento}
         const sql = 'insert into atendimentos set ?'
 
-        conexao.query(sql, atendimento, (erro) => {
+        conexao.query(sql, atendimentoDatado, (erro) => {
             if(erro){
                 console.log(erro)
             } else {
-                console.log(sql)
+                console.log('O insert deu certo:', atendimentoDatado)
             }
         })
     }
